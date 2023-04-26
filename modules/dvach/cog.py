b2b4203a17203da1793c8cc69c8edf91d28f119a
rlul <45661770+rlul@users.noreply.github.com>
2023-04-26 20:42:37 +0300

    Switched to using webhooks, changed user hashing method

--- modules/dvach/cog.py
+++ modules/dvach/cog.py
@@ -1,9 +1,10 @@
 import discord
 from discord.utils import escape_mentions
 from discord.ext import commands
+import discord_webhook
 import hashlib
 
-from config import CONFIG, EMOJI_LIST, FLAG_LIST, PREFIX
+from config import CONFIG, EMOJI_LIST, AVATAR_LIST, PREFIX
 from utils import get_message_attachments, dm_to_mentioned_user
 
 
@@ -19,6 +20,7 @@ class Dvach(commands.Cog, name="2ch"):
         self.dvach_channel = self.bot.get_channel(
             int(CONFIG['Server']['DVACH_CHANNEL_ID'])
         )
+        self.dvach_webhook = str(CONFIG['Server']['DVACH_CHANNEL_WEBHOOK'])
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -31,16 +33,16 @@ class Dvach(commands.Cog, name="2ch"):
             if not message.content.startswith(PREFIX):
                 # экранируем упоминания
                 content = escape_mentions(message.content)
-                # Хэшируем имя пользователя и присваеваем ему псевдоним в виде эмодзи и флага (хуйня))
-                hash = int( hashlib.sha1(message.author.name.encode("utf-8")).hexdigest(), 16)
+                # Хэшируем имя пользователя и присваеваем ему псевдоним
+                salt = int(CONFIG['Bot']['SALT'])
+                hash = int(hashlib.sha1(str(message.author.id^salt).encode("utf-8")).hexdigest(), 16)
                 emoji = EMOJI_LIST[hash % (10 ** 3) % len(EMOJI_LIST)]
-                flag = FLAG_LIST[hash % (10 ** 4) % len(FLAG_LIST)]
-
+                webhook_username = str(hash % (10 ** 4)) + emoji
+                webhook_avatar = AVATAR_LIST[hash % (10 ** 3) % len(AVATAR_LIST)]
+                webhook = discord_webhook.DiscordWebhook(url=self.dvach_webhook, username=webhook_username, avatar_url=webhook_avatar)
+                webhook.content = content + '\n' + '\n'.join(get_message_attachments(message))
                 # Отправляем сообщение в 2ch канал
-                await self.dvach_channel.send(
-                    f'Аноним ({emoji}:{flag}): {content}\n' +
-                    '\n'.join(get_message_attachments(message))
-                )
+                webhook.execute()
 
 
 async def setup(bot: commands.Bot):
